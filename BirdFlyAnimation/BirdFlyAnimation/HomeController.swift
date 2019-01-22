8371feb6f78ccc2198d2047ddaf9c8dc69d631d4
ChuongMD <chuongmd91@gmail.com>
2019-01-22 10:29:26 +0700

    start

---
+++ BirdFlyAnimation/BirdFlyAnimation/HomeController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BirdFlyAnimation
+//
+//  Created by Mai Dinh Chuong on 1/22/19.
+//  Copyright © 2019 Mai Dinh Chuong. All rights reserved.
+//
+
+import UIKit
+
+class HomeController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
